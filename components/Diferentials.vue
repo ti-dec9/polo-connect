9ffe3a23db5e77849e2e35b44a4400c74088ddf4
xploter13 <xploter13@gmail.com>
2023-03-31 17:23:56 -0300

    Inserção icones diferenciais

--- components/Diferentials.vue
+++ components/Diferentials.vue
@@ -9,7 +9,7 @@
       <div class="row text-center" style="margin-top: 80px;">
         <div class="col-lg-3 g-0">
           <div class="card shadow-none card-diferentials h-100">
-            <img src="" class="card-img-top" alt="">
+            <img src="https://ik.imagekit.io/8z4sc6x4w/Dec9/Construtora_Polo/icons/porta-digital.svg?updatedAt=1680293891685" style="width: 80px; height: auto; margin: 0 auto;" class="card-img-top" alt="">
             <div class="card-body">
               <p>Fachadura Digital</p>
             </div>
@@ -17,7 +17,7 @@
         </div>
         <div class="col-lg-3 shadow-none g-0">
           <div class="card card-diferentials h-100">
-            <img src="" class="card-img-top" alt="">
+            <img src="https://ik.imagekit.io/8z4sc6x4w/Dec9/Construtora_Polo/icons/usb.svg?updatedAt=1680293891778" style="width: 80px; height: auto; margin: 0 auto;" class="card-img-top" alt="">
             <div class="card-body">
               <p>Tomadas USB</p>
             </div>
@@ -25,7 +25,7 @@
         </div>
         <div class="col-lg-3 shadow-none g-0">
           <div class="card card-diferentials h-100">
-            <img src="" class="card-img-top" alt="">
+            <img src="https://ik.imagekit.io/8z4sc6x4w/Dec9/Construtora_Polo/icons/electric-car.svg?updatedAt=1680293891765" style="width: 80px; height: auto; margin: 0 auto;" class="card-img-top" alt="">
             <div class="card-body">
               <p>Caregadores para veículos elétricos</p>
             </div>
@@ -33,7 +33,7 @@
         </div>
         <div class="col-lg-3 g-0">
           <div class="card shadow-none card-diferentials h-100">
-            <img src="" class="card-img-top" alt="">
+            <img src="https://ik.imagekit.io/8z4sc6x4w/Dec9/Construtora_Polo/icons/wifi-signal.svg?updatedAt=1680293891660" style="width: 80px; height: auto; margin: 0 auto;" class="card-img-top" alt="">
             <div class="card-body">
               <p>Rede wi-fi livre nas áreas comuns</p>
             </div>
@@ -41,7 +41,7 @@
         </div>
         <div class="col-lg-3 g-0">
           <div class="card shadow-none card-diferentials">
-            <img src="" class="card-img-top" alt="">
+            <img src="https://ik.imagekit.io/8z4sc6x4w/Dec9/Construtora_Polo/icons/elevador.svg?updatedAt=1680293891783" style="width: 80px; height: auto; margin: 0 auto;" class="card-img-top" alt="">
             <div class="card-body">
               <p>Elevadores com sistema regenerativo</p>
             </div>
